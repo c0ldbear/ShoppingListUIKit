c6d1ae9f644c7e2b8a6f78ff76441fe779666a21
teddy <teddy.juhlin-henricson@bontouch.com>
2022-08-12 11:33:23 +0200

    Add tab bar, 2 view controllers as a start

---
+++ ShoppingListPro/ItemListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SomeViewController.swift
+//  ShoppingListPro
+//
+//  Created by teddy juhlin-henricson on 2022-08-12.
+//
+
+import UIKit
+
+class ItemListViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPurple
+        listLayout()
+    }
+    
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = true
+        listConfiguration.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25)
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+}
